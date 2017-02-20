b53e75f7e9a4f1fb623fe5957317123d9489ace7
Arnav Gupta <championswimmer@gmail.com>
2017-02-21 05:09:37 +0530

    printing this fucking weird pattern
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- 1-2-1-pattern.js
+++ 1-2-1-pattern.js
@@ -1,3 +1,34 @@
 /**
  * Created by championswimmer on 21/2/17.
  */
+/*
+Print
+ 1
+ 11
+ 21
+ 1211
+ 111221
+ */
+
+var n = 5;
+
+var s = "1";
+for (var i = 1; i < n; i++) {
+  var a =Array.from(s);
+  var c = a[0];
+  var d = 1;
+  var sn = "";
+  for (var j = 1; j < a.length; j++) {
+    if (a[j] != c) {
+      sn = sn + d + c;
+      c = a[j];
+      d = 1;
+    } else {
+      d++;
+    }
+  }
+  sn = sn + d + c;
+  s = sn;
+}
+
+console.log(s)
